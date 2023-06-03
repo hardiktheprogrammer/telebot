5291eb2f3a48f43d1c2c82090bddc3e94587698c
hardikthecoder <85208662+hardiktheprogrammer@users.noreply.github.com>
2023-06-03 18:35:07 +0530

    add API

--- bot2/app.js
+++ bot2/app.js
@@ -1,5 +1,6 @@
 const { Telegraf } = require('telegraf');
 const { message } = require('telegraf/filters');
+const axios = require('axios');
 
 const bot = new Telegraf('6293237322:AAEetnBq0F9ItJB3cx5CyBavukwuuVJ5I38');
 
@@ -32,5 +33,27 @@ bot.command('alert', (ctx) => {
     },
   });
 });
+
+bot.action('Check', (ctx) => {
+  ctx.deleteMessage();
+  ctx.telegram.sendMessage(ctx.chat.id, ' Today Market Check', {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          { text: 'Coingecko', callback_data: 'Coingecko' },
+          { text: 'Coinmarketcap', callback_data: 'Coinmarketcap' },
+        ],
+        [{ text: 'Binance', callback_data: 'Binance' }],
+      ],
+    },
+  });
+});
+
+function getdata() {
+  url = 'https://pro-api.coinmarketcap.com/v1/exchange/market-pairs/latest';
+  axios.get(url).then((res) => {
+    console.log(res.data);
+  });
+}
 
 bot.launch();
